e1a867ea66ee929bc8366743d5a0a47ae8cc49f9
Tatsuro <tatsuro.secure.mail@protonmail.ch>
2023-03-04 23:19:02 +0900

    fix

--- app/src/main/java/com/tatsuro/app/actionsandroid/ui/main/MainViewModel.kt
+++ app/src/main/java/com/tatsuro/app/actionsandroid/ui/main/MainViewModel.kt
@@ -2,6 +2,4 @@ package com.tatsuro.app.actionsandroid.ui.main
 
 import androidx.lifecycle.ViewModel
 
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
+class MainViewModel : ViewModel()
